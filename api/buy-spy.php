3d2c78fd5f9681de4191efc7482c43f7d3531e78
Smarty Indonesia <smartyindo@gmail.com>
2026-03-16 14:58:43 +0700

    update spy connection

--- api/buy-spy.php
+++ api/buy-spy.php
@@ -1,8 +1,22 @@
 <?php 
-$curl='https://api.coingecko.com/api/v3/coins/smarty-pay?localization=false&tickers=true&market_data=false&community_data=false&developer_data=false&sparkline=false';
+$url='https://api.coingecko.com/api/v3/simple/price?ids=smarty-pay&vs_currencies=idr%2Cusd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true';
 
-$ch = file_get_contents($curl);
-$data = json_decode($ch, true);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// PENTING: Tambahkan User-Agent agar tidak kena 403
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
+$response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+// curl_close($ch);
+
+if ($httpCode == 200) {
+    $data = json_decode($response, true);
+    // print_r($data);
+} else {
+    echo "Gagal mengambil data. Error Code: " . $httpCode;
+}
 
 
 $pagetitle = 'Buy SPY'; 
