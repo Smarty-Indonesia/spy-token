a2129afb05ddffb8cd14d9d22c56a819e6c1ffef
Smarty Indonesia <smartyindo@gmail.com>
2026-03-16 18:43:46 +0700

    update button

--- api/buy-spy.php
+++ api/buy-spy.php
@@ -75,13 +75,14 @@ $pagetitle = 'Buy SPY';
               <p style="font-style: italic">(update: off since 2023)</p>
               <!-- <p>Today's Price: <?php echo "USD ".number_format(($data['tickers']['1']['converted_last']['usd']), 4, ".", ",") ?></p>
               <p>Volume: <?php echo number_format(($data['tickers']['1']['volume']), 2, ".", ",") ?> ~ <?php echo "USD ".number_format(($data['tickers']['1']['converted_volume']['usd']), 2, ".", ",") ?></p> -->
-			        <a class="btn-disabled scrollto">Buy | USDT</a>
+			        <a href="" class="btn-disabled scrollto">Buy | USDT</a>
             </div>
           </div>
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="300">
             <div class="icon-box">
               <img src="../assets/img/buy/cointiger.png" style="width:250px; border-radius:20px">
-              <h4>www.cointiger.com <br> (off since 2024)</h4>
+              <h4>www.cointiger.com</h4>
+              <p style="font-style: italic">(update: off since 2024)</p>
               <!-- <p>Today's Price: <?php echo "USD ".number_format(($data['tickers']['3']['converted_last']['usd']), 4, ".", ",") ?></p>
               <p>Volume: <?php echo number_format(($data['tickers']['3']['volume']), 2, ".", ",") ?> ~ <?php echo "USD ".number_format(($data['tickers']['3']['converted_volume']['usd']), 2, ".", ",") ?></p> -->
 			        <a href="#" class="btn-get-started scrollto">Buy | USDT</a>
@@ -107,14 +108,16 @@ $pagetitle = 'Buy SPY';
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
             <div class="icon-box">
               <img src="../assets/img/buy/hotbit.png" style="width:250px; border-radius:20px">
-              <h4>www.hotbit.io <br>(closed: 2023)</h4>
+              <h4>www.hotbit.io</h4>
+              <p style="font-style: italic">(update: off since 2023)</p>
 			        <a href="#" class="btn-get-started scrollto">Buy | USDT</a>
             </div>
           </div>
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch mt-4 mt-xl-0" data-aos="zoom-in" data-aos-delay="400">
             <div class="icon-box">
               <img src="../assets/img/buy/bitforex.png" style="width:250px; border-radius:20px">
-              <h4>www.bitforex.com <br> (off since: 2024)</h4>
+              <h4>www.bitforex.com</h4>
+              <p style="font-style: italic">(update: off since 2024)</p>
 			        <a href="#" class="btn-get-started scrollto">Buy | USDT</a>
             </div>
           </div>
@@ -139,7 +142,8 @@ $pagetitle = 'Buy SPY';
           <div class="col-xl-3 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
             <div class="icon-box">
               <img src="../assets/img/buy/digiasset.jpg" style="width:250px; border-radius:20px">
-              <h4>www.digiassetindo.com <br> (off since: 2023)</h4>
+              <h4>www.digiassetindo.com</h4>
+              <p style="font-style: italic">(update: off since 2023)</p>
               <a href="#" class="btn-get-started scrollto">Buy | IDR</a>
               <a href="#" class="btn-get-started scrollto">Buy | USDT</a>
             </div>
